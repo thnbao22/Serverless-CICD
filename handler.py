91a14bd6d2edaed059aec1c0aff1e4b162e4a5be
Bao Truong <baotruong@AP-0821-1030.local>
2024-10-16 08:41:00 +0700

    update print event

--- handler.py
+++ handler.py
@@ -5,6 +5,7 @@ def hello(event, context):
     body = {
         "message": "Go Serverless v4.0! Your function executed successfully!",
     }
+    print(event)
 
     response = {"statusCode": 200, "body": json.dumps(body)}
 
